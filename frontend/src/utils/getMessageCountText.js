f8b571aeddacd1d9bd37594ac15db75ab7f5fd42
masha yanson <mashayanson@gmail.com>
2024-07-10 14:49:38 +0300

    deleted getMessageCount

--- frontend/src/utils/getMessageCountText.js
+++
@@ -1,6 +0,0 @@
-const getMessageCountText = (count) => {
-  const cases = [2, 0, 1, 1, 1, 2];
-  const titles = ['сообщение', 'сообщения', 'сообщений'];
-  return titles[(count % 100 > 4 && count % 100 < 20) ? 2 : cases[Math.min(count % 10, 5)]];
-};
-export default getMessageCountText;
