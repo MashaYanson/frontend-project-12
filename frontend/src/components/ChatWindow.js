8cedd889d113186cb50d8dce06731541b033f31c
masha yanson <mashayanson@gmail.com>
2024-08-21 15:59:43 +0300

    try to fix warnings with useMemo

--- frontend/src/components/ChatWindow.js
+++ frontend/src/components/ChatWindow.js
@@ -1,88 +1,3 @@
-// /* eslint-disable max-len */
-// import React from 'react';
-// import { useSelector } from 'react-redux';
-// import { useTranslation } from 'react-i18next';
-// import { Formik, Form, Field } from 'formik';
-// import filter from 'leo-profanity';
-// import useInstance from '../utils/axios';
-// import routes from '../routes';
-//
-// const ChatWindow = ({ channel }) => {
-//   const { t } = useTranslation();
-//   const { name } = channel;
-//   const instance = useInstance();
-//   const userName = useSelector((state) => state.user.userData.username);
-// eslint-disable-next-line max-len
-//   const messages = useSelector((state) => state.messages.data.filter((msg) => channel.id === msg.channelId));
-//
-//   return (
-//     <div className="d-flex flex-column h-100">
-//       <div className="bg-light mb-4 p-3 shadow-sm small">
-//         <b>
-//           #
-//           {name}
-//         </b>
-//         <div>
-//           {t('messagesCounter.count', { count: messages.length })}
-//         </div>
-//       </div>
-//       <div className="chat-messages overflow-auto px-5">
-//         {messages.map((msg) => (
-//           <div key={msg.id}>
-//             <b>{msg.username}</b>
-//             :
-//             {' '}
-//             {msg.body}
-//           </div>
-//         ))}
-//       </div>
-//       <div className="mt-auto px-5 py-3">
-//         <Formik
-//           initialValues={{ message: '' }}
-//           onSubmit={(values, { resetForm }) => {
-// eslint-disable-next-line max-len
-//             const newMessage = { body: filter.clean(values.message), channelId: channel.id, username: userName };
-//             instance({ method: 'post', url: routes.api.messagesPath(), data: newMessage })
-//               .then(() => {
-//                 resetForm();
-//               });
-//           }}
-//         >
-//           {({ handleSubmit, isSubmitting }) => (
-//             <Form noValidate="" className="py-1 border rounded-2" onSubmit={handleSubmit}>
-//               <div className="input-group has-validation">
-//                 <Field
-//                   name="message"
-//                   aria-label={t('newMessage')}
-//                   placeholder={t('send')}
-//                   className="border-0 p-0 ps-2 form-control"
-//                 />
-//                 <button type="submit" disabled={isSubmitting} className="btn btn-group-vertical">
-//                   <svg
-//                     xmlns="http://www.w3.org/2000/svg"
-//                     viewBox="0 0 16 16"
-//                     width="20"
-//                     height="20"
-//                     fill="currentColor"
-//                   >
-//                     <path
-//                       fillRule="evenodd"
-// eslint-disable-next-line max-len
-//                       d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"
-//                     />
-//                   </svg>
-//                   <span className="visually-hidden">{t('send')}</span>
-//                 </button>
-//               </div>
-//             </Form>
-//           )}
-//         </Formik>
-//       </div>
-//     </div>
-//   );
-// };
-//
-// export default ChatWindow;
 import React, { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
@@ -97,10 +12,9 @@ const ChatWindow = ({ channel }) => {
   const instance = useInstance();
   const userName = useSelector((state) => state.user.userData.username);
 
+  const messagesData = useSelector((state) => state.messages.data);
   // eslint-disable-next-line max-len
-  const messages = useSelector((state) => state.messages.data.filter((msg) => channel.id === msg.channelId));
-
-  const memoizedMessages = useMemo(() => messages, [messages]);
+  const messages = useMemo(() => messagesData.filter((msg) => channel.id === msg.channelId), [channel.id, messagesData]);
 
   return (
     <div className="d-flex flex-column h-100">
@@ -110,11 +24,11 @@ const ChatWindow = ({ channel }) => {
           {name}
         </b>
         <div>
-          {t('messagesCounter.count', { count: memoizedMessages.length })}
+          {t('messagesCounter.count', { count: messages.length })}
         </div>
       </div>
       <div className="chat-messages overflow-auto px-5">
-        {memoizedMessages.map((msg) => (
+        {messages.map((msg) => (
           <div key={msg.id}>
             <b>{msg.username}</b>
             :
