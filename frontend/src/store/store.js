d99cb8e165a1a1c0a70a215d263b674fc2057af5
masha yanson <mashayanson@gmail.com>
2024-08-27 22:26:01 +0300

    fixed bug

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,12 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { actionsReducer as messagesSlice } from '@reduxjs/toolkit/src/tests/utils/helpers';
 import userReducer from './userSlice';
 import channelReducer from './channelSlice';
-import messagesReducer from './messageSlice';
+import messagesReducer, { deleteChannelMessages } from './messageSlice';
 
 const channelMessagesMiddleware = (store) => (next) => (action) => {
   if (action.type === 'channels/removeChannel') {
-    store.dispatch(messagesSlice.actions.deleteChannelMessages({ channelId: action.payload }));
+    store.dispatch(deleteChannelMessages({ channelId: action.payload }));
   }
   return next(action);
 };
