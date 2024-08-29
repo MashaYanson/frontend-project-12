39f9a2c391e661a7d020278283a36b3e601e825d
masha yanson <mashayanson@gmail.com>
2024-08-29 13:12:19 +0300

    added extraReduser

--- frontend/src/store/channelSlice.js
+++ frontend/src/store/channelSlice.js
@@ -1,3 +1,58 @@
+// /* eslint-disable no-param-reassign */
+// import { createSelector, createSlice } from '@reduxjs/toolkit';
+// import { deleteChannelMessages } from './messageSlice';
+//
+// const DEFAULT_CHANNEL_ID = '1';
+//
+// const initialState = { data: [], channelId: DEFAULT_CHANNEL_ID };
+//
+// export const channelSlice = createSlice({
+//   name: 'channels',
+//   initialState,
+//   reducers: {
+//     updateChannels: (state, action) => {
+//       state.data = action.payload;
+//     },
+//     addChannel: (state, action) => {
+//       const isChannelExist = state.data.some((item) => item.id === action.payload.id);
+//       if (!isChannelExist) {
+//         state.data.push(action.payload);
+//       }
+//     },
+//     removeChannel: (state, action) => {
+//       state.data = state.data.filter((channel) => channel.id !== action.payload);
+//       deleteChannelMessages(state, action);
+//       // state.data = state.data.filter((message) => action.channelId !== message.channelId);
+//       if (state.channelId === action.payload) {
+//         state.channelId = DEFAULT_CHANNEL_ID;
+//       }
+//     },
+//     editChannel: (state, action) => {
+//       const elIndex = state.data.findIndex((item) => item.id === action.payload.id);
+//       state.data[elIndex] = action.payload;
+//     },
+//     setChannel: (state, action) => {
+//       state.channelId = action.payload;
+//     },
+//   },
+// });
+// export const selectChannelNames = createSelector(
+//   (state) => state.channels.data,
+//   (channels) => channels.map((channel) => channel.name),
+// );
+//
+// export const selectChannelById = createSelector(
+//   (state) => state.channels.data,
+//   (_, channelId) => channelId,
+//   (channels, channelId) => channels.find((item) => item.id === channelId),
+// );
+//
+// export const {
+//   updateChannels, addChannel, removeChannel, setChannel, editChannel,
+// } = channelSlice.actions;
+//
+// export default channelSlice.reducer;
+
 /* eslint-disable no-param-reassign */
 import { createSelector, createSlice } from '@reduxjs/toolkit';
 import { deleteChannelMessages } from './messageSlice';
@@ -19,14 +74,6 @@ export const channelSlice = createSlice({
         state.data.push(action.payload);
       }
     },
-    removeChannel: (state, action) => {
-      state.data = state.data.filter((channel) => channel.id !== action.payload);
-      deleteChannelMessages(state, action);
-      // state.data = state.data.filter((message) => action.channelId !== message.channelId);
-      if (state.channelId === action.payload) {
-        state.channelId = DEFAULT_CHANNEL_ID;
-      }
-    },
     editChannel: (state, action) => {
       const elIndex = state.data.findIndex((item) => item.id === action.payload.id);
       state.data[elIndex] = action.payload;
@@ -35,7 +82,18 @@ export const channelSlice = createSlice({
       state.channelId = action.payload;
     },
   },
+  extraReducers: (builder) => {
+    builder.addCase('channels/removeChannel', (state, action) => {
+      state.data = state.data.filter((channel) => channel.id !== action.payload);
+      // Удаление сообщений для канала через экстра редюсер
+      deleteChannelMessages(state, action);
+      if (state.channelId === action.payload) {
+        state.channelId = DEFAULT_CHANNEL_ID;
+      }
+    });
+  },
 });
+
 export const selectChannelNames = createSelector(
   (state) => state.channels.data,
   (channels) => channels.map((channel) => channel.name),
@@ -48,7 +106,7 @@ export const selectChannelById = createSelector(
 );
 
 export const {
-  updateChannels, addChannel, removeChannel, setChannel, editChannel,
+  updateChannels, addChannel, setChannel, editChannel,
 } = channelSlice.actions;
 
 export default channelSlice.reducer;
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,21 +1,35 @@
+// import { configureStore } from '@reduxjs/toolkit';
+// import userReducer from './userSlice';
+// import channelReducer from './channelSlice';
+// import messagesReducer, { deleteChannelMessages } from './messageSlice';
+//
+// const channelMessagesMiddleware = (store) => (next) => (action) => {
+//   if (action.type === 'channels/removeChannel') {
+//     store.dispatch(deleteChannelMessages({ channelId: action.payload }));
+//   }
+//   return next(action);
+// };
+// const createStore = () => configureStore({
+//   reducer: {
+//     user: userReducer,
+//     channels: channelReducer,
+//     messages: messagesReducer,
+//   },
+//   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(channelMessagesMiddleware),
+//
+// });
+// export default createStore;
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './userSlice';
 import channelReducer from './channelSlice';
-import messagesReducer, { deleteChannelMessages } from './messageSlice';
+import messagesReducer from './messageSlice';
 
-const channelMessagesMiddleware = (store) => (next) => (action) => {
-  if (action.type === 'channels/removeChannel') {
-    store.dispatch(deleteChannelMessages({ channelId: action.payload }));
-  }
-  return next(action);
-};
 const createStore = () => configureStore({
   reducer: {
     user: userReducer,
     channels: channelReducer,
     messages: messagesReducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(channelMessagesMiddleware),
-
 });
+
 export default createStore;
